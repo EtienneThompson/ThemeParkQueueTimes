447f612ee8bf568119de00adb6b7a0974d71d01d
Etienne Thompson <43585499+EtienneThompson@users.noreply.github.com>
2023-12-29 10:23:16 -0700

    Add API proxy for getting queue times. (#4)

--- theme-park-queue-times-sdk/src/index.ts
+++ theme-park-queue-times-sdk/src/index.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 import { QueueTimes, ThemePark } from "./types";
 
-export async function getParkList(): Promise<ThemePark[]> {
+/**
+ * Gets the list of available theme parks and individual parks within.
+ * @returns ThemePark[] - list of theme parks and individual parks.
+ */
+export async function getThemeParkList(): Promise<ThemePark[]> {
   let response = await axios.get("https://queue-times.com/parks.json");
   if (response.status !== 200) {
     throw new Error("Failed to get park list");
@@ -10,11 +14,21 @@ export async function getParkList(): Promise<ThemePark[]> {
   return response.data as ThemePark[];
 }
 
+/**
+ * Get the list of lands and queue times for each ride within that land.
+ * @param parkId The id of the park to get data for.
+ * @returns QueueTimes - object containing park id, land, and ride data.
+ */
 export async function getQueueTimes(parkId: number): Promise<QueueTimes> {
   let response = await axios.get(`https://queue-times.com/parks/${parkId}/queue_times.json`);
   if (response.status !== 200) {
     throw new Error("Failed to get queue time");
   }
 
-  return response.data as QueueTimes;
+  return {
+    park_id: parkId,
+    ...response.data,
+  } as QueueTimes;
 }
+
+export * from "./types";
